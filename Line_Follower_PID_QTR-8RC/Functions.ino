778a5a7028c91489d2ed20d4c502289d0854d222
Andrew <andrewkfa@outlook.com>
2023-05-04 13:00:14 -0300

    New code
    Clean code and a small but important bugfix.

--- Line_Follower_PID_QTR-8RC/Functions.ino
+++ Line_Follower_PID_QTR-8RC/Functions.ino
@@ -1,19 +1,27 @@
 void speed(int mL, int mR) {
-  analogWrite(h[0], abs(mL));
+  analogWrite(hBridge[0], abs(mL));
   if(mL >=0) {
-    digitalWrite(h[1], HIGH);
-    digitalWrite(h[2], LOW);
+    digitalWrite(hBridge[1], HIGH);
+    digitalWrite(hBridge[2], LOW);
   } else{
-    digitalWrite(h[1], LOW);
-    digitalWrite(h[2], HIGH);
+    digitalWrite(hBridge[1], LOW);
+    digitalWrite(hBridge[2], HIGH);
   }
-  analogWrite(h[5], abs(mR));
+  analogWrite(hBridge[5], abs(mR));
   if(mR >=0) {
-    digitalWrite(h[3], HIGH);
-    digitalWrite(h[4], LOW);
+    digitalWrite(hBridge[3], HIGH);
+    digitalWrite(hBridge[4], LOW);
   } else{
-    digitalWrite(h[3], LOW);
-    digitalWrite(h[4], HIGH);
+    digitalWrite(hBridge[3], LOW);
+    digitalWrite(hBridge[4], HIGH);
+  }
+}
+
+int readSensors() {
+  if(isLineBlack) {
+    return qtr.readLineBlack(sensorValues);
+  }else{
+    return qtr.readLineWhite(sensorValues);
   }
 }
 
@@ -23,5 +31,35 @@ void printposition() {
     Serial.print(sensorValues[i]);
     Serial.print('\t');
   }
-  Serial.println(position);
+  Serial.println(readSensors());
 }
+
+void calibration() {
+  pinMode(LED_BUILTIN, OUTPUT); 
+  digitalWrite(LED_BUILTIN, HIGH); //Turn on LED to indicate calibration
+
+  while(millis() <= calibTime*1000) { 
+    qtr.calibrate();
+  }
+  digitalWrite(LED_BUILTIN, LOW); 
+
+  for (uint8_t i = 0; i < SensorCount; i++) // print the calibration minimum values
+  {
+    Serial.print(qtr.calibrationOn.minimum[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+
+  for (uint8_t i = 0; i < SensorCount; i++)//Print the calibration maximum values 
+  {
+    Serial.print(qtr.calibrationOn.maximum[i]);
+    Serial.println(' ');
+  }
+}
+
+void forwardOverride(int margin) {
+    if (error >= -margin and error <= margin) {
+      lSpeed = maxSpeed;
+      rSpeed = maxSpeed;
+    }
+  }
--- Line_Follower_PID_QTR-8RC/Line_Follower_PID_QTR-8RC.ino
+++ Line_Follower_PID_QTR-8RC/Line_Follower_PID_QTR-8RC.ino
@@ -1,6 +1,16 @@
-//Data de início: 20/04/2023
-//Autor: Andrew Kennedy
-//Seguidor de linha PID utilizando o sensor Pololu QTR-8RC
+//Started: 20/04/2023, updated: 03/05/2023
+//Author: Andrew Kennedy
+//Line Follower PID with the Pololu QTR-8RC sensor
+
+//------------------PID Control-------------------
+float Kp=1;
+float Ki=0.002;
+float Kd=1.5;
+
+const byte maxSpeed = 255; 
+bool isLineBlack = true;
+byte calibTime = 3; //Calibration time in seconds
+//-------------------------------------------------
 
 #include <QTRSensors.h>
 QTRSensors qtr;
@@ -8,77 +18,39 @@ QTRSensors qtr;
 const uint8_t SensorCount = 8;
 uint16_t sensorValues[SensorCount];
 
-float error=0, P=0, I=0, D=0, PID=0;
-float prevError=0, prevI=0;
-uint16_t position = 0;
-const byte initial_motor_speed = 180; 
+//H Bridge          [ENA~,IN1,IN2,IN3,IN4,ENB~]
+const byte hBridge[] = {10,A0,A1,A2,A3,11};
 
-//PID Control
-float Kp=4;
-float Ki=0.002;
-float Kd=2;
+float P=0, I=0, D=0, PID=0, error=0, lastError=0;
+int lSpeed, rSpeed;
 
-//H Bridge       [ENA~,IN1,IN2,IN3,IN4,ENB~]
-const byte h[] = {10,A0,A1,A2,A3,11};
+void setup() {
+  Serial.begin(9600);
 
-void setup()
-{
-  // configure the sensors
+  //Configure the sensors
   qtr.setTypeRC();
   qtr.setSensorPins((const uint8_t[]){2, 3, 4, 5, 6, 7, 8, 9}, SensorCount);
-
-//Calibration---------------------------------
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH); // turn on Arduino's LED to indicate we are in calibration mode
-
-  // 2.5 ms RC read timeout (default) * 10 reads per calibrate() call
-  // = ~25 ms per calibrate() call.
-  // Call calibrate() 400 times to make calibration take about 10 seconds.
-  for (uint16_t i = 0; i < 400; i++)
-  {
-    qtr.calibrate();
-  }
-  digitalWrite(LED_BUILTIN, LOW); // turn off Arduino's LED to indicate we are through with calibration
-//----------------------------------------------
-
-
-  // print the calibration minimum values measured when emitters were on
-  Serial.begin(9600);
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(qtr.calibrationOn.minimum[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-
-  // print the calibration maximum values measured when emitters were on
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(qtr.calibrationOn.maximum[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-  Serial.println();
+  calibration();
 }
 
 void loop() {
   //Read sensors
-  position = qtr.readLineBlack(sensorValues);
-  error = map(position, 0, 7000, -100, 100);
+  error = map(readSensors(), 0, 7000, -1000, 1000);
 
   //Calculate PID
   P = error;
   I = I + error;
-  D = error-prevError;
+  D = error - lastError;
   PID = (Kp*P) + (Ki*I) + (Kd*D);
-  prevError = error;
+  lastError = error;
 
   //Control Motors
-  int left_motor_speed = initial_motor_speed + PID;
-  int right_motor_speed = initial_motor_speed - PID;
+  lSpeed = maxSpeed + PID;
+  rSpeed = maxSpeed - PID;
 
-  left_motor_speed = constrain(left_motor_speed, 0, 255);
-  right_motor_speed = constrain(right_motor_speed, 0, 255);
+  lSpeed = constrain(lSpeed, -maxSpeed, maxSpeed);
+  rSpeed = constrain(rSpeed, -maxSpeed, maxSpeed);
+  forwardOverride(20);
 
-  speed(left_motor_speed, right_motor_speed);
+  speed(lSpeed, rSpeed);
 }
