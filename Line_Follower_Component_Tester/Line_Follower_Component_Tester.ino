66cd8a397cf08353b09adeb09ce43060b15e7386
Silent <121576911+Silent0Wings@users.noreply.github.com>
2023-07-21 18:11:02 -0400

    Magic_Numbers

--- Line_Follower_Component_Tester/Line_Follower_Component_Tester.ino
+++ Line_Follower_Component_Tester/Line_Follower_Component_Tester.ino
@@ -15,23 +15,32 @@ const uint8_t PIN_BUTTON = 35;
 const uint8_t PIN_LED = 15;
 const uint8_t PIN_MARKER_SENSOR = 36;
 
+// delays values
+const int Setup_Delay=100;
+const int auxSensorTest_Delay
+
+//setup
+const int Begin_value=115200;
 
 const int PAUSE_TIME = 3000;
 long speed = 100; 
 
 //Setup of the module of sensors
 const uint8_t SENSOR_COUNT = 8;       // The number of sensors, which should match the length of the pins array
+const uint8_t[] QTR_8RC={ 17, 16, 18, 5, 23, 19, 22, 21 }
 uint16_t sensorValues[SENSOR_COUNT];  // An array in which to store the calibrated sensor readings
 
-
+//Motor turning values
+const int Motor_Turn1 = 90;
+const int Motor_Turn2 = 30;
 
 
 void setup() {
-  Serial.begin(115200);
-  delay(100);
+  Serial.begin(Begin_value);
+  delay(Setup_Delay);
 
   qtr.setTypeRC();  // For QTR-8RC      Sensor pins:
-  qtr.setSensorPins((const uint8_t[]){ 17, 16, 18, 5, 23, 19, 22, 21 }, SENSOR_COUNT);
+  qtr.setSensorPins(QTR_8RC, SENSOR_COUNT);
 
   pinMode(PIN_MARKER_SENSOR, INPUT);
 }
@@ -43,7 +52,7 @@ void loop() {
 }
 
 void auxSensorTest(){
-  delay(250);
+  delay(auxSensorTest_Delay);
   Serial.println(digitalRead(PIN_MARKER_SENSOR));
   Serial.println(analogRead(PIN_MARKER_SENSOR));
 }
@@ -95,7 +104,7 @@ void vespaTestMotors(){
   motor.stop();
   
   Serial.println("Turn");
-  motor.turn(90,30);
+  motor.turn(Motor_Turn1,Motor_Turn2);
   delay(PAUSE_TIME);
   motor.stop();
   
